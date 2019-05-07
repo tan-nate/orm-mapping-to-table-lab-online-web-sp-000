3d0153af1b6a8186987378796b97003046d1f157
Nate Tan <nktan93@gmail.com>
2019-05-07 00:33:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
-  attr_accessor :name, :album, :id
+  attr_accessor :name, :grade, :id
   
-  
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
   
 end
